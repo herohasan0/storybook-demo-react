d2b1adb798307b81f211f81b3e15d8cc94a8f87f
Hasan KAHRAMAN <hasankahraman@hasan-air.local>
2022-08-14 10:29:06 +0300

    add bar chart as a story

---
+++ src/components/Charts/BarChart/BarChart.jsx
@@ -0,0 +1,143 @@
+import { Bar } from 'react-chartjs-2'
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  PointElement,
+  LineElement
+} from 'chart.js'
+import PropTypes from 'prop-types'
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  PointElement,
+  LineElement
+)
+const BarChart = ({
+  x_axis,
+  y_values,
+  title,
+  horizontal,
+  x_grid,
+  y_grid,
+  rounded,
+  show_legend,
+  show_y_labels,
+  padding,
+  colors
+}) => {
+  // Vertical bar chart options
+  const options = {
+    responsive: true,
+    maintainAspectRatio: false,
+    scales: {
+      y: {
+        type: 'linear',
+        display: show_y_labels,
+        position: 'left',
+        grid: {
+          display: y_grid,
+          drawBorder: false
+        }
+      },
+      x: {
+        grid: {
+          display: x_grid
+        }
+      }
+    },
+    plugins: {
+      legend: {
+        display: show_legend,
+        position: 'top'
+      }
+    },
+    layout: {
+      padding: padding
+    }
+  }
+
+  if (horizontal === true) {
+    options['indexAxis'] = 'y' // if horizontal is true set index axis to y
+    delete options['scales']['y']['type']
+  }
+
+  return (
+    <Bar
+      data={{
+        labels: x_axis,
+        datasets: [
+          {
+            label: title,
+            data: y_values,
+            backgroundColor: colors,
+            yAxisID: 'y',
+            borderRadius: rounded === true ? 50 : 0
+          }
+        ]
+      }}
+      options={options}
+    />
+  )
+}
+
+BarChart.propTypes = {
+  /**
+   * Chart title.
+   */
+  title: PropTypes.string,
+  /**
+   * Toggle grids for X axis
+   */
+  x_grid: PropTypes.bool,
+  /**
+   * Toggle grids for Y axis
+   */
+  y_grid: PropTypes.bool,
+  rounded: PropTypes.bool,
+  horizontal: PropTypes.bool,
+  /**
+   * Toggle legend title for the chart
+   */
+  show_legend: PropTypes.bool,
+  /**
+   * Toggle Y axis labels
+   */
+  show_y_labels: PropTypes.bool,
+  padding: PropTypes.number,
+  x_axis: PropTypes.array.isRequired,
+  y_values: PropTypes.array.isRequired,
+  colors: PropTypes.array
+}
+
+BarChart.defaultProps = {
+  x_grid: true,
+  y_grid: true,
+  rounded: false,
+  horizontal: false,
+  show_legend: true,
+  show_y_labels: true,
+  padding: 60,
+  colors: [
+    '#0D68FC',
+    '#CA4AFF',
+    '#FF9600',
+    '#1B998B',
+    '#6235CC',
+    '#FF5971',
+    '#2A19EF',
+    '#8D3782',
+    '#F6692C'
+  ]
+}
+
+export default BarChart
---
+++ src/components/Charts/BarChart/BarChart.stories.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+import BarChart from "./BarChart";
+
+export default {
+  title: "Charts/BarChart",
+  component: BarChart,
+  argTypes: { x_grid: { control: "boolean" }, horizontal: { control: " " } },
+};
+
+const Template = (args) => (
+  <div className="h-screen">
+    <BarChart {...args} />
+  </div>
+);
+
+export const Vertical = Template.bind({});
+
+Vertical.args = {
+  title: "Vertical BarChart",
+  x_axis: [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ],
+  y_values: [15, 13, 14, 15, 13, 32, 10],
+};
+
+export const Horizontal = Template.bind({});
+
+Horizontal.args = {
+  title: "Vertical BarChart",
+  x_axis: [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ],
+  y_values: [15, 13, 14, 15, 13, 32, 10],
+  horizontal: true,
+};
