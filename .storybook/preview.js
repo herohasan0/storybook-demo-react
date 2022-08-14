4fc4580d5be783fb426b671bcee75c774c2cea79
Hasan KAHRAMAN <hasankahraman@hasan-air.local>
2022-08-14 10:26:20 +0300

    expand actions tab as default

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,6 +2,7 @@ import "../src/styles.css";
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
+    expanded: true,
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
